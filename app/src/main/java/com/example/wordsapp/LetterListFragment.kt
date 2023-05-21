74f9736edf56a6cd16b7ae27d3c5da410660f3c0
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2023-05-21 11:10:29 -0500

    Aplicación con múltiples vistas usando fragmentos

---
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -0,0 +1,41 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LetterListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LetterListFragment : Fragment() {
+
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstance: Bundle?): View? {
+
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstance: Bundle?) {
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
